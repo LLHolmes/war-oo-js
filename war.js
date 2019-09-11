3bccab227bfaf7ae8301c6577f175b77a4ebbc36
LLHolmes <lana.lee.holmes@gmail.com>
2019-09-10 20:48:44 -0400

    Create Deck class and constructor function to build deck

--- war.js
+++ war.js
@@ -2,25 +2,29 @@ console.log("The Game Of WAR!")
 
 const cardList = [{ '2': 2 }, { '3': 3 }, { '4': 4 }, { '5': 5 }, { '6': 6 }, { '7': 7 }, { '8': 8 }, { '9': 9 }, { '10': 10 }, { 'Jack': 11 }, { 'Queen': 12 }, { 'King': 13 }, { 'Ace': 14 }];
 
-let deck = [];
-
 class Card {
   constructor(name, points, suit) {
     this.name = name;
     this.points = points;
     this.suit = suit;
-
-    deck.push(this);
   };
 };
 
-const buildDeck = (cardList) => {
-  cardList.forEach(card => {
-    let clubs = new Card(Object.keys(card)[0], Object.values(card)[0], 'clubs');
-    let spades = new Card(Object.keys(card)[0], Object.values(card)[0], 'spades');
-    let hearts = new Card(Object.keys(card)[0], Object.values(card)[0], 'hearts');
-    let diamonds = new Card(Object.keys(card)[0], Object.values(card)[0], 'diamonds');
-  });
+class Deck {
+  constructor(cardList) {
+    this.cards = [];
+    cardList.forEach(card => {
+      this.cards.push(new Card(Object.keys(card)[0], Object.values(card)[0], 'clubs'));
+      this.cards.push(new Card(Object.keys(card)[0], Object.values(card)[0], 'spades'));
+      this.cards.push(new Card(Object.keys(card)[0], Object.values(card)[0], 'hearts'));
+      this.cards.push(new Card(Object.keys(card)[0], Object.values(card)[0], 'diamonds'));
+    });
+  };
 };
 
-buildDeck(cardList)
+
+
+
+
+// Game play:
+let deck = new Deck(cardList)
