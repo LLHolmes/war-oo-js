c3d32770ea69f168e7213e613235e3649b5a3e40
LLHolmes <lana.lee.holmes@gmail.com>
2019-09-10 21:55:11 -0400

    Create Player class and constructor function and create two players

--- war.js
+++ war.js
@@ -2,6 +2,13 @@ console.log("The Game Of WAR!")
 
 const cardList = [{ '2': 2 }, { '3': 3 }, { '4': 4 }, { '5': 5 }, { '6': 6 }, { '7': 7 }, { '8': 8 }, { '9': 9 }, { '10': 10 }, { 'Jack': 11 }, { 'Queen': 12 }, { 'King': 13 }, { 'Ace': 14 }];
 
+class Player {
+  constructor(name) {
+    this.name = name;
+    this.pile = [];
+  };
+};
+
 class Card {
   constructor(name, points, suit) {
     this.name = name;
@@ -38,5 +45,8 @@ class Deck {
 
 
 // Game play:
+let playerOne = new Player('Player One')
+let playerTwo = new Player('Player Two')
 let deck = new Deck(cardList)
+
 deck.shuffle()
